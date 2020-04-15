61d0287d3c39f123523c640d643b5617c56b2618
rachidba <rachidbaaziz.contact@gmail.com>
2020-04-15 01:46:18 +0100

    Update unit tests

--- src/test/java/ma/lmentor/restapi/mappers/MentorMapperTest.java
+++ src/test/java/ma/lmentor/restapi/mappers/MentorMapperTest.java
@@ -1,6 +1,7 @@
 package ma.lmentor.restapi.mappers;
 
 import ma.lmentor.restapi.entities.Mentor;
+import ma.lmentor.restapi.entities.User;
 import ma.lmentor.restapi.models.MentorCreationDto;
 import ma.lmentor.restapi.models.MentorDetailsDto;
 import ma.lmentor.restapi.models.MentorItemDto;
@@ -18,6 +19,7 @@ public class MentorMapperTest {
     @Test
     public void creationDtoToMentorTest() {
         Integer mentorId = null;
+        var user = new User();
         String firstName = "Rachid";
         String lastName = "BAAZIZ";
         String email = "rachidbaaziz.contact@gmail.com";
@@ -26,8 +28,7 @@ public class MentorMapperTest {
         String description = "A description";
         double sessionPrice = 100;
         var mentorData = new MentorCreationDto(firstName, lastName, email, phoneNumber, title, description, sessionPrice);
-//        var expectedMentor = new Mentor(mentorId, firstName, lastName, email, phoneNumber, title, description, sessionPrice);
-        var expectedMentor = new Mentor();
+        var expectedMentor = new Mentor(mentorId, user, firstName, lastName, email, phoneNumber, title, description, sessionPrice);
         var mentor = mentorMapper.toMentor(mentorData);
         Assertions.assertEquals(expectedMentor, mentor);
     }
@@ -35,6 +36,7 @@ public class MentorMapperTest {
     @Test
     public void mentorToItemTest() {
         Integer mentorId = 1;
+        var user = new User();
         String firstName = "Rachid";
         String lastName = "BAAZIZ";
         String email = "rachidbaaziz.contact@gmail.com";
@@ -42,8 +44,7 @@ public class MentorMapperTest {
         String title = "Software engineer";
         String description = "A description";
         double sessionPrice = 100;
-//        var mentor = new Mentor(mentorId, firstName, lastName, email, phoneNumber, title, description, sessionPrice);
-        var mentor = new Mentor();
+        var mentor = new Mentor(mentorId, user, firstName, lastName, email, phoneNumber, title, description, sessionPrice);
         var expectedMentorItem = new MentorItemDto(mentorId, firstName + lastName, title, sessionPrice);
         var mentorItem = mentorMapper.toMentorItem(mentor);
         Assertions.assertEquals(expectedMentorItem, mentorItem);
@@ -52,6 +53,7 @@ public class MentorMapperTest {
     @Test
     public void mentorToDetailsTest() {
         Integer mentorId = 1;
+        var user = new User();
         String firstName = "Rachid";
         String lastName = "BAAZIZ";
         String email = "rachidbaaziz.contact@gmail.com";
@@ -59,8 +61,7 @@ public class MentorMapperTest {
         String title = "Software engineer";
         String description = "A description";
         double sessionPrice = 100;
-//        var mentor = new Mentor(mentorId, firstName, lastName, email, phoneNumber, title, description, sessionPrice);
-        var mentor = new Mentor();
+        var mentor = new Mentor(mentorId, user, firstName, lastName, email, phoneNumber, title, description, sessionPrice);
         var expectedMentorDetails = new MentorDetailsDto(mentorId, firstName + lastName, email, phoneNumber, title, description, sessionPrice);
         var mentorItem = mentorMapper.toMentorDetails(mentor);
         Assertions.assertEquals(expectedMentorDetails, mentorItem);
