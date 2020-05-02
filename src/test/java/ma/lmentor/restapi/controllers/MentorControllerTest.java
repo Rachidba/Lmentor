22d407c857ee9c547d0c39ccfb22dc8ec7724a29
rachidba <rachidbaaziz.contact@gmail.com>
2020-05-02 02:11:26 +0000

    Add integration tests to test rest controllers

---
+++ src/test/java/ma/lmentor/restapi/controllers/MentorControllerTest.java
@@ -0,0 +1,70 @@
+package ma.lmentor.restapi.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import ma.lmentor.restapi.models.EducationCreationDto;
+import ma.lmentor.restapi.models.ExperienceCreationDto;
+import ma.lmentor.restapi.models.GenderType;
+import ma.lmentor.restapi.models.MentorCreationDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.HashSet;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class MentorControllerTest extends AbstractTest {
+    @Override
+    @BeforeAll
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    public void mentorCreation() throws Exception {
+        var experienceCreationDto = new ExperienceCreationDto();
+        experienceCreationDto.setCompanyName("Societe generale");
+        experienceCreationDto.setRole("R&D Software engineer");
+        experienceCreationDto.setDescription("It was cool");
+        experienceCreationDto.setStartYear(2019);
+        experienceCreationDto.setStartMonth(8);
+
+        var experiences = new HashSet<ExperienceCreationDto>();
+        experiences.add(experienceCreationDto);
+
+        var educationCreationDto = new EducationCreationDto();
+        educationCreationDto.setSchool("FST Mohammedia");
+        educationCreationDto.setDegree("Master degree");
+        educationCreationDto.setStartYear(2016);
+        educationCreationDto.setEndYear(2019);
+        educationCreationDto.setFieldOfStudy("Software engineering");
+
+        var educations = new HashSet<EducationCreationDto>();
+        educations.add(educationCreationDto);
+
+        var mentorCreationDto = new MentorCreationDto();
+        mentorCreationDto.setGender(GenderType.GENDER_MALE);
+        //mentorCreationDto.setFirstName("Rachid");
+        mentorCreationDto.setLastName("BAAZIZ");
+        mentorCreationDto.setEmail("rachidbaaziz.contact@gmail.com");
+        mentorCreationDto.setTitle("Software engineer");
+        mentorCreationDto.setCity("Casablanca");
+        mentorCreationDto.setJob("Employee");
+        mentorCreationDto.setExpertiseField("Information technology");
+        mentorCreationDto.setPhoneNumber("06125478");
+        mentorCreationDto.setExperiences(experiences);
+        mentorCreationDto.setEducations(educations);
+
+        var url = "/api/mentors";
+        var inputJson = super.mapToJson(mentorCreationDto);
+        var mvcResult = this.mvc.perform(MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson))
+                .andReturn();
+
+        var response = mvcResult.getResponse();
+        Assertions.assertEquals(400, response.getStatus());
+    }
+}
---
+++ src/test/java/ma/lmentor/restapi/controllers/UserControllerTest.java
@@ -0,0 +1,86 @@
+package ma.lmentor.restapi.controllers;
+
+import ma.lmentor.restapi.models.*;
+import org.junit.jupiter.api.*;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.HashMap;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserControllerTest extends AbstractTest {
+
+    @Override
+    @BeforeAll
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    public void register_shouldReturn400_whenNonExistingFields() throws Exception {
+        /*
+
+        */
+
+        var registerDto = new RegistrationDto();
+        registerDto.setUsername("rachidba");
+        registerDto.setPassword("password");
+
+        var url = "/api/register";
+        var inputJson = super.mapToJson(registerDto);
+        var mvcResult = this.mvc.perform(MockMvcRequestBuilders.post(url)
+                                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                .content(inputJson))
+                            .andReturn();
+
+        var response = mvcResult.getResponse();
+        Assertions.assertEquals(400, response.getStatus());
+
+        var responseContent = response.getContentAsString();
+        var errors = super.mapFromJson(responseContent, HashMap.class);
+        Assertions.assertEquals(2, errors.size());
+        Assertions.assertTrue(errors.containsKey("username"));
+        Assertions.assertTrue(errors.containsKey("role"));
+    }
+
+    @Test
+    public void register_shouldReturn400_whenInvalidEmail() throws Exception {
+        var registerDto = new RegistrationDto();
+        registerDto.setUsername("rachidba");
+        registerDto.setPassword("password");
+        registerDto.setRole(RoleType.ROLE_MENTOR);
+
+        var url = "/api/register";
+        var inputJson = super.mapToJson(registerDto);
+        var mvcResult = this.mvc.perform(MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson))
+                .andReturn();
+
+        var response = mvcResult.getResponse();
+        Assertions.assertEquals(400, response.getStatus());
+
+        var responseContent = response.getContentAsString();
+        var errors = super.mapFromJson(responseContent, HashMap.class);
+        Assertions.assertEquals(1, errors.size());
+        Assertions.assertTrue(errors.containsKey("username"));
+    }
+
+    @Test
+    public void register_shouldReturn200_whenValidEmail() throws Exception {
+        var registerDto = new RegistrationDto();
+        registerDto.setUsername("rachidba@gmail.com");
+        registerDto.setPassword("password");
+        registerDto.setRole(RoleType.ROLE_MENTOR);
+
+        var url = "/api/register";
+        var inputJson = super.mapToJson(registerDto);
+        var mvcResult = this.mvc.perform(MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson))
+                .andReturn();
+
+        var response = mvcResult.getResponse();
+        Assertions.assertEquals(200, response.getStatus());
+    }
+}
