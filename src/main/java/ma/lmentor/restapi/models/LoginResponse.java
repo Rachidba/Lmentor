31d6cac1d28b52bfcff77966ce6331c8a59d0be6
rachidba <rachidbaaziz.contact@gmail.com>
2020-05-23 05:22:44 +0000

    Add login response DTOs

---
+++ src/main/java/ma/lmentor/restapi/models/LoginResponse.java
@@ -0,0 +1,16 @@
+package ma.lmentor.restapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class LoginResponse {
+    private Integer profileId;
+    private String fullName;
+    private String jwToken;
+    private String email;
+    private RoleType role;
+}
---
+++ src/main/java/ma/lmentor/restapi/models/MentorLoginResponse.java
@@ -0,0 +1,22 @@
+package ma.lmentor.restapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class MentorLoginResponse extends LoginResponse {
+    private boolean isProfileCompleted;
+
+    public MentorLoginResponse(Integer profileId,
+                               String fullName,
+                               String jwToken,
+                               String email,
+                               RoleType role,
+                               boolean isProfileCompleted) {
+        super(profileId, fullName, jwToken, email, role);
+        this.isProfileCompleted = isProfileCompleted;
+    }
+}
