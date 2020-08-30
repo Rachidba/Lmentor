9366fa3fa7071e65760e94f49446155253d07f1f
rachidba <rachidbaaziz.contact@gmail.com>
2020-08-30 03:28:00 +0100

    [Front] Filter mentors by category

--- web-client/src/app/components/profile-creation/mentor-profile-creation/mentor-profile-creation.component.spec.ts
+++ web-client/src/app/components/profile-creation/mentor-profile-creation/mentor-profile-creation.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MentorProfileCreationComponent } from './mentor-profile-creation.component';
 
 describe('MentorProfileCreationComponent', () => {
